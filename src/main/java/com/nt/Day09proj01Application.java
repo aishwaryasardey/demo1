82160bbaa69c35171d11c22e12cd30813cca9f54
aishwarya <aishwaryasardey24@gmail.com>
2024-06-20 20:11:17 +0530

    print statement

--- src/main/java/com/nt/Day09proj01Application.java
+++ src/main/java/com/nt/Day09proj01Application.java
@@ -13,6 +13,7 @@ public class Day09proj01Application {
 		ConfigurableApplicationContext ctx =SpringApplication.run(Day09proj01Application.class, args);
 		Employee emp=ctx.getBean("emp",Employee.class);
 		System.out.println(emp);
+		System.out.println("employee");
 		
 	}
 
